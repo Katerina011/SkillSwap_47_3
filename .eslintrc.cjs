4e2111f867001d720551eb58474b5c7856f474b4
PecherskiyD <pecherskiy@vector-best.ru>
2026-04-02 09:32:11 +0700

    Переписал кнопку. Сделал фасад. Пробросли стандартные для кнопки пропсы. Пробросил ref.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,5 +33,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
     'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
