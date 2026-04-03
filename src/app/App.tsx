abe539fbb6eca22b82454569d067242e9a9ad3e5
RechkalovAA <rechkalov.a.a@ya.ru>
2026-04-03 22:38:44 +0500

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Avatar } from '../shared/ui/Avatar';
-
 export function App() {
   return (
     <main>
       <h1>SkillSwap_47_3</h1>
       <p>Делимся навыками — растём вместе.</p>
-        <Avatar size="md" name="Test User" />
     </main>
   );
 }
