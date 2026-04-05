f7145c4d23b25d97879792d09ea572573b41c224
Orlov Anton <obucenia232323@gmail.com>
2026-04-05 16:33:47 +0500

    files with prettier

--- src/pages/LoginPage/LoginHeader.tsx
+++ src/pages/LoginPage/LoginHeader.tsx
@@ -15,19 +15,19 @@ function LoginHeader() {
         <Link to="/" className={styles['close-button']}>
           <span className={styles['close-text']}>Закрыть</span>
           <div className={styles['close-icon-wrapper']}>
-            <svg 
-              className={styles['close-icon']} 
-              width="24" 
-              height="24" 
-              viewBox="0 0 24 24" 
-              fill="none" 
+            <svg
+              className={styles['close-icon']}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path 
-                d="M18 6L6 18M6 6L18 18" 
-                stroke="#253017" 
-                strokeWidth="2" 
-                strokeLinecap="round" 
+              <path
+                d="M18 6L6 18M6 6L18 18"
+                stroke="#253017"
+                strokeWidth="2"
+                strokeLinecap="round"
               />
             </svg>
           </div>
