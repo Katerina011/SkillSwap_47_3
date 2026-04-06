793e0e54cad2524b42950eb4e1a90dbc9090f2c7
Leonard-Cherepanov <cherepanov.leonard@yandex.ru>
2026-04-06 15:37:17 +0300

    Контракт + загрузка моков (users.json / skills.json)

---
+++ src/features/catalog/index.ts
@@ -0,0 +1,3 @@
+export { useCatalogSources } from './hooks/useCatalogSources';
+export { catalogItemFixtures } from './model/catalogItemFixtures';
+export type { CatalogItem, CatalogItemKind } from './model/types';
